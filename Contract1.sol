633697bdf3fdb20b65a19eb3566231ea78e76054
Swarupa Saha <swarupasaha@Swarupas-MacBook-Air.local>
2026-05-18 18:53:53 +0530

    something

--- Contract1.sol
+++ Contract1.sol
@@ -1,2 +1,12 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.30; //version
+
+contract SimpleStorage{
+    
+    uint public favoriteNumber; //bu default the value will be 0
+    
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        favoriteNumber = favoriteNumber+12;
+    }
+}
