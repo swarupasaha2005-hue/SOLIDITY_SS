b458a232f8d296702d235b61e3c1627930334e5f
Swarupa Saha <swarupasaha@Swarupas-MacBook-Air.local>
2026-05-24 23:06:30 +0530

    solidity

--- Contract1.sol
+++ Contract1.sol
@@ -3,11 +3,34 @@ pragma solidity 0.8.30; //version
 
 contract SimpleStorage{
     
-    uint public favoriteNumber; //bu default the value will be 0
+    uint public myfavoriteNumber; //by default the value will be 0
+
+    //uint256[] listOfFavoriteNumbers;
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //dynamic array
+    Person[] public listOfPeople;
+
+    //gives(maps) the favorite number of the given string
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    // Person public swa = Person({favoriteNumber:8, name:"Swarupa"});
+    // Person public aba = Person(10, "Abantika");
     
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-        favoriteNumber = favoriteNumber+12;
-        favoriteNumber = favoriteNumber+123;
+        myfavoriteNumber = _favoriteNumber;
     }
+
+    //view, pure
+    function retrieve() public view returns(uint256){
+        return myfavoriteNumber;
+    }
+
+      function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));  
+        nameToFavoriteNumber[_name] =_favoriteNumber;
+      }
 }
